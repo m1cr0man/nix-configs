96705f24b59783af16abfd214d161d364b06a4e7
Lucas Savva <lucas@m1cr0man.com>
2019-06-11 20:05:50 +0100

    Dont redirect acme to https

--- common/httpd.nix
+++ common/httpd.nix
@@ -2,6 +2,7 @@
   httpUpgrade =  ''
     RewriteEngine On
     RewriteCond %{HTTPS} off
+    RewriteCond %{REQUEST_URI} !^/.well-known/.*$ [NC]
     RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R=301,L]
     ProxyPass /\.well-known/ http://localhost/.well-known/
   '';
