d2f682ec5e01f78c900e503cdaa6c8d5f86873ee
Lucas Savva <lucas@m1cr0man.com>
2019-06-11 20:37:16 +0100

    Simplify https vhosts as much as possible

--- common/httpd.nix
+++ common/httpd.nix
@@ -2,8 +2,8 @@
   httpUpgrade =  ''
     RewriteEngine On
     RewriteCond %{HTTPS} off
-    RewriteCond %{REQUEST_URI} !^/.well-known/.*$ [NC]
-    RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R=301,L]
-    ProxyPass /\.well-known/ http://localhost/.well-known/
+    RewriteCond %{REQUEST_URI} !^/\.well-known/.*$ [NC]
+    RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R=301]
+    ProxyPass /.well-known !
   '';
 }
