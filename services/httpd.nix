8a5f494ab24032499b5b2deebbd0f3c8a8e2e82e
Lucas Savva <lucas@m1cr0man.com>
2019-06-11 19:50:13 +0100

    More ssl shenanigans

--- services/httpd.nix
+++ services/httpd.nix
@@ -3,18 +3,13 @@ let
   certsDir = "/var/lib/acme";
   webrootDir = certsDir + "/.webroot";
 in {
-  security.acme.certs."acme.m1cr0man.com" = {
+  security.acme.certs."*.m1cr0man.com" = {
     email = "lucas+acme@m1cr0man.com";
     webroot = webrootDir;
-    extraDomains = {
-      "m1cr0man.com" = null;
-      "u.m1cr0man.com" = null;
-      "www.m1cr0man.com" = null;
-      "s3.m1cr0man.com" = null;
-    };
     postRun = "systemctl reload httpd.service";
   };
   security.acme.directory = certsDir;
+  security.acme.production = false;
 
   services.httpd = {
     enable = true;
@@ -24,16 +19,13 @@ in {
     sslServerKey = "${certsDir}/m1cr0man.com/key.pem";
     sslServerCert = "${certsDir}/m1cr0man.com/fullchain.pem";
 
-    virtualHosts = [{
-      hostName = "acme.m1cr0man.com";
-      servedDirs = [{
-        urlPath = "/.well-known/acme-challenge";
-        dir = "${webrootDir}/.well-known/acme-challenge";
-      }];
-    }];
-
     adminAddr = "lucas+httpd@m1cr0man.com";
-    hostName = "localhost";
+    hostName = "m1cr0man.com";
+
+    servedDirs = [{
+      urlPath = "/.well-known/acme-challenge";
+      dir = "${webrootDir}/.well-known/acme-challenge";
+    }];
 
     listen = [{ port = 80; } { port = 443; }];
   };
