11bc176daf41dcd6ffed59878ca4d4d0567649f1
Lucas Savva <lucas@m1cr0man.com>
2019-06-11 21:00:33 +0100

    Enable ssl on minio vhost

--- services/minio.nix
+++ services/minio.nix
@@ -21,6 +21,7 @@ in {
 
   security.acme.certs."m1cr0man.com".extraDomains."s3.m1cr0man.com" = null;
   services.httpd.virtualHosts = [{
+    enableSSL = true;
     hostName = "s3.m1cr0man.com";
     extraConfig = httpdCommon.httpUpgrade + "ProxyPass / http://127.0.0.1:9000/";
   }];
