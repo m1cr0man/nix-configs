0d65fce9900f4c297840897544d5f18840656318
Lucas Savva <lucas@m1cr0man.com>
2019-06-13 22:32:39 +0100

    Generalise httpd, add cragglerock certs

--- services/httpd.nix
+++ services/httpd.nix
@@ -1,14 +1,36 @@
 { config, pkgs, ... }:
 let
   certsDir = "/var/lib/acme";
+
   webrootDir = certsDir + "/.webroot";
-in {
-  security.acme.certs."m1cr0man.com" = {
+
+  acmeCert = {
     email = "lucas+acme@m1cr0man.com";
     webroot = webrootDir;
     postRun = "systemctl reload httpd.service";
   };
+
+  acmeVhost = domain: {
+      hostName = domain;
+      serverAliases = [ "*.${domain}" ];
+      servedDirs = [{
+        urlPath = "/.well-known/acme-challenge";
+        dir = "${webrootDir}/.well-known/acme-challenge";
+      }];
+
+      extraConfig = ''
+        RewriteEngine On
+        RewriteCond %{HTTPS} off
+        RewriteCond %{REQUEST_URI} !^/\.well-known/.*$ [NC]
+        RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R=301]
+      '';
+    };
+in {
   security.acme.directory = certsDir;
+  security.acme.certs = {
+    "m1cr0man.com" = acmeCert;
+    "cragglerock.cf" = acmeCert;
+  };
 
   services.httpd = {
     enable = true;
@@ -21,27 +43,16 @@ in {
       ProxyPreserveHost On
     '';
 
-    # Only acme certs are accessible via port 80,
-    # everything else is explicitly upgraded to https
-    virtualHosts = [{
-      hostName = "m1cr0man.com";
-      serverAliases = [ "*.m1cr0man.com" ];
-      servedDirs = [{
-        urlPath = "/.well-known/acme-challenge";
-        dir = "${webrootDir}/.well-known/acme-challenge";
-      }];
-
-      extraConfig = ''
-        RewriteEngine On
-        RewriteCond %{HTTPS} off
-        RewriteCond %{REQUEST_URI} !^/\.well-known/.*$ [NC]
-        RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R=301]
-      '';
-    }];
+    virtualHosts = [
+      acmeVhost "m1cr0man.com"
+      acmeVhost "cragglerock.cf"
+    ];
 
     adminAddr = "lucas+httpd@m1cr0man.com";
     hostName = "localhost";
 
+    # Only acme certs are accessible via port 80,
+    # everything else is explicitly upgraded to https
     listen = [{ port = 80; }];
   };
 
