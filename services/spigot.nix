bd6b117d45a316b175985ad38c56981fc6ddb9d3
Lucas Savva <lucas@m1cr0man.com>
2019-06-15 12:58:26 +0100

    Use proper variable references

--- services/spigot.nix
+++ services/spigot.nix
@@ -7,7 +7,8 @@ runCommand "spigot-jar-1.14.2" {
         sha256 = "b27b683c9f30f22f56726af23350b9d6d870921443798b38adb341aead91d08c";
     };
 } ''
-export PATH="$git/bin:$jdk/bin:$PATH"
-java -jar BuildTools.jar --rev 1.14.2
+export PATH="${git}/bin:${jdk}/bin:$PATH"
+export HOME=$PWD
+java -jar $buildtools --rev 1.14.2
 mv spigot-*.jar $out
 ''
