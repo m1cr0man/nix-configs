ee5293a9cc03f16f5913dff494d9bbcf79f18809
Lucas Savva <lucas@m1cr0man.com>
2019-06-15 01:10:20 +0100

    Add custom spigot builder

--- services/minecraft.nix
+++ services/minecraft.nix
@@ -10,10 +10,7 @@ in {
     package = pkgs.minecraft-server.overrideAttrs (oldAttrs: {
       name = "minecraft-server-1.14.2.spigot";
       version = "1.14.2.spigot";
-      src = pkgs.fetchurl {
-        url = "https://cdn.getbukkit.org/spigot/spigot-1.14.2.jar";
-        sha256 = "924173542d6064ec72eb22ff717123892d555d8e9dfe72a10b2a83f58599480e";
-      };
+      src = import ./spigot.nix;
     });
 
     dataDir = "/zstorage/craig_mc";
---
+++ services/spigot.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+
+runCommandCC "spigot-jar-1.14.2" {
+    inherit jdk git;
+    buildtools = fetchurl {
+        url = "https://hub.spigotmc.org/jenkins/job/BuildTools/101/artifact/target/BuildTools.jar";
+        sha256 = "b27b683c9f30f22f56726af23350b9d6d870921443798b38adb341aead91d08c";
+    };
+} ''
+export PATH="$git/bin:$jdk/bin:$PATH"
+java -jar BuildTools.jar --rev 1.14.2
+mv spigot-*.jar $out
+'';
