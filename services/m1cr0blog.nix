dacafb4568276e57cef0eee5a34074e70f2c3d77
Lucas Savva <lucas@m1cr0man.com>
2019-06-15 13:04:11 +0100

    Add m1cr0blog deployment

---
+++ services/m1cr0blog.nix
@@ -0,0 +1,26 @@
+let
+    m1cr0blog = import ../packages/m1cr0blog.nix;
+in {
+  systemd.services.m1cr0blog = {
+    description = "Personal blog";
+    after = [ "network.target" "zfs-import.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    environment = { NODE_ENV = "production"; };
+
+    serviceConfig = {
+        ExecStart = "${pkgs.nodejs-11_x}/bin/node ${m1cr0blog}/dist/index.js";
+        User = "m1cr0blog";
+        Restart = "always";
+        WorkingDirectory = "/zstorage/m1cr0blog";
+    };
+  };
+
+  security.acme.certs."m1cr0man.com".extraDomains."www.m1cr0man.com" = null;
+  services.httpd.virtualHosts = [{
+    enableSSL = true;
+    hostName = "m1cr0man.com";
+    serverAliases = [ "www.m1cr0man.com" ];
+    extraConfig = "ProxyPass / http://127.0.0.1:3000/";
+  }];
+}
