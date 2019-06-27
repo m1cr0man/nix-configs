17a1623b75ab86d34a027d5681aa9834c1836ccd
Lucas Savva <lucas@m1cr0man.com>
2019-06-27 23:15:24 +0100

    Use spigot in mc server

--- services/minecraft.nix
+++ services/minecraft.nix
@@ -10,7 +10,10 @@ in {
     package = pkgs.minecraft-server.overrideAttrs (oldAttrs: {
       name = "minecraft-server-1.14.2.spigot";
       version = "1.14.2.spigot";
-      src = import ./spigot.nix;
+      src = pkgs.fetchurl {
+        url = "https://cdn.getbukkit.org/spigot/spigot-1.14.2.jar";
+        sha256 = "924173542d6064ec72eb22ff717123892d555d8e9dfe72a10b2a83f58599480e";
+      };
     });
 
     dataDir = "/zstorage/craig_mc";
