2441bd652af39b75109bda35bc711d03f6ea08b5
Lucas Savva <lucas@m1cr0man.com>
2019-10-07 23:11:07 +0100

    Fix nextcloud config, make use of their .htaccess

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -19,8 +19,6 @@ in {
   users.extraUsers.nextcloud.group = lib.mkForce "wwwrun";
   users.groups.nginx.members = [ "wwwrun" "nextcloud" ];
 
-  # services.phpfpm.pools.nextcloud.group = "wwwrun";
-
   services.nextcloud = {
     inherit hostName home;
     enable = true;
@@ -29,11 +27,6 @@ in {
       adminpass = secrets.nextcloud_adminpass;
       overwriteProtocol = "https";
     };
-    # poolSettings = {
-    #   "listen.owner" = "wwwrun";
-    #   "listen.group" = "wwwrun";
-    # };
-
   };
 
   security.acme.certs."m1cr0man.com".extraDomains."${hostName}" = null;
@@ -45,57 +38,31 @@ in {
       RewriteEngine On
       SSLProxyEngine on
       DeflateCompressionLevel 4
+      Header always unset X-Powered-By
+      Header always set Strict-Transport-Security 15552000
+      Alias "/store-apps" "${home}/store-apps"
+
+      AddOutputFilterByType DEFLATE application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy
 
       <FilesMatch \.php$>
-          SetHandler "proxy:unix:/run/phpfpm-nextcloud/nextcloud.sock|fcgi://localhost/"
+        SetHandler "proxy:unix:/run/phpfpm-nextcloud/nextcloud.sock|fcgi://localhost/"
       </FilesMatch>
 
-      <IfModule mod_env.c>
-        SetEnv front_controller_active true
-      </IfModule>
+      <Directory "${pkgs.nextcloud}">
+        Options -Indexes +FollowSymLinks
+        AllowOverride All
+        Require all granted
+      </Directory>
 
-      <IfModule mod_dir.c>
-        DirectorySlash off
-      </IfModule>
-
-      <Directory ${home}>
-        Options +Indexes +FollowSymLinks
+      <Directory "${home}">
+        Options -Indexes +FollowSymLinks
         AllowOverride None
         Require all granted
       </Directory>
 
-      <Location "^/+(build|tests|config|lib|3rdparty|templates|data|autotest|occ|issue|indie|db_|console)/*">
-        AllowOverride None
-        Order deny,allow
-        Deny from All
-      </Location>
-
-      <FilesMatch "^.*\.(css|js|woff2?|svg|gif)$">
-        Options +FollowSymLinks
-        ${header "Cache-Control" "public, max-age=15778643"}
-      </FilesMatch>
-
-      ${header "X-Content-Type-Options" "nosniff"}
-      ${header "X-XSS-Protection" "1; mode=block"}
-      ${header "X-Robots-Tag" "none"}
-      ${header "X-Download-Options" "noopen"}
-      ${header "X-Permitted-Cross-Domain-Policies" "none"}
-      ${header "Referrer-Policy" "no-referrer"}
-      Header always unset X-Powered-By
-
-      AddOutputFilterByType DEFLATE application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
-
-      RewriteRule ^/\.well-known/(card|cal)dav/?$ %{REQUEST_SCHEME}/%{HTTP_HOST}/remote.php/dav [END,R=301]
-      RewriteRule ^/(updater|ocs-provider|ocm-provider)$ %{REQUEST_URI}/ [END]
-
-      RewriteRule ^/store-app/(.*) ${home}/$1 [P,QSA,END]
-
-      # Equivalent of try-files
-      RewriteCond %{DOCUMENT_ROOT}/%{REQUEST_URI} !-f
-      RewriteRule ^(?!/[^\./]+\.php).*$ /index.php%{REQUEST_URI} [P,QSA,END]
-
-      RewriteCond %{REQUEST_URI} /(?:index|remote|public|cron|core/ajax\\/update|status|ocs\\/v[12]|updater\\/.+|ocs-provider\\/.+|ocm-provider\\/.+)\\.php/?$
-      RewriteRule ^(?!/[^\./]+\.php).*$ /index.php%{REQUEST_URI} [P,QSA,END]
+      <IfModule mod_dir.c>
+        DirectorySlash off
+      </IfModule>
     '';
   }];
 }
