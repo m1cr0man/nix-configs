5a32151b502d0bac9088c9ae8f31084eee298327
Lucas Savva <lucas@m1cr0man.com>
2020-05-30 16:02:08 +0100

    Remove all traefik config

--- services/minio.nix
+++ services/minio.nix
@@ -11,13 +11,6 @@ in {
     region = "EU";
   };
 
-  services.traefik.configOptions.backends.minio_back.servers.gelandewagen.url = "http://localhost:9000";
-  services.traefik.configOptions.frontends.minio = {
-    backend = "minio_back";
-    passHostHeader = true;
-    routes.m1s3.rule = "Host:s3.m1cr0man.com";
-  };
-
   security.acme.certs."m1cr0man.com".extraDomains."s3.m1cr0man.com" = null;
   services.httpd.virtualHosts."s3.m1cr0man.com" = {
     onlySSL = true;
--- services/traefik.nix
+++
@@ -1,6 +0,0 @@
-{
-  services.traefik.enable = true;
-  services.traefik.configOptions.file.watch = true;
-  services.traefik.configOptions.defaultEntryPoints = [ "http" ];
-  services.traefik.configOptions.entryPoints.http.address = ":80";
-}
