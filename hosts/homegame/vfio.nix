46128018d387c2730c2982b3ff3d09e878f2e778
Lucas Savva <lucas@m1cr0man.com>
2020-09-26 18:44:15 +0100

    Add USB3 controller to vfio

--- hosts/homegame/vfio.nix
+++ hosts/homegame/vfio.nix
@@ -2,7 +2,7 @@
   boot.kernelModules = [ "vfio" "vfio-pci" "vfio_virqfd" "vfio_iommu_type1" ];
   boot.blacklistedKernelModules = [ "nvidia" "nouveau" "snd_ctxfi" "snd_hda_intel" ];
   boot.extraModprobeConfig = ''
-    options vfio-pci ids=1102:000b,8086:1d20,10de:13c2,10de:0fbb
+    options vfio-pci ids=1102:000b,8086:1d20,10de:13c2,10de:0fbb,1033:0194
     options vfio_iommu_type1 allow_unsafe_interrupts=1
   '';
   # options kvm ignore_msrs=1
