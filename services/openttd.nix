ec7908739ac31ee7a658fc5ee24f12546b79b157
Lucas Savva <lucas@m1cr0man.com>
2020-10-25 12:18:29 +0000

    Use nixpkgs openttd

--- services/openttd.nix
+++ services/openttd.nix
@@ -1,12 +1,5 @@
 { pkgs, ... }:
-let
-  openttd = pkgs.openttd.overrideAttrs (old: {
-    src = pkgs.fetchurl {
-      url = "https://cdn.openttd.org/openttd-releases/1.10.1/openttd-1.10.1-source.tar.xz";
-      sha256 = "0d22a3c50f7a321f4f211594f4987ac16c381e8e3e40f116848e63e91e7fbb9b";
-    };
-  });
-in {
+{
   users.users.openttd = {
     description = "Service user for openttd";
     isSystemUser = true;
@@ -22,7 +15,7 @@ in {
     environment = { NODE_ENV = "production"; };
 
     script = ''
-      ${openttd}/bin/openttd -D
+      ${pkgs.openttd}/bin/openttd -D
     '';
 
     serviceConfig = {
