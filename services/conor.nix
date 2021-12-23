045d833d172349a2ba54c5bad9d08d8a4aa9a02c
Lucas Savva <lucas@m1cr0man.com>
2021-12-23 14:02:44 +0000

    foundry.conor: Add websocket proxy support

--- services/conor.nix
+++ services/conor.nix
@@ -3,6 +3,16 @@
   services.httpd.virtualHosts."foundry.conor.m1cr0man.com" = {
     forceSSL = true;
     useACMEHost = "m1cr0man.com";
-    extraConfig = "ProxyPass / http://127.0.0.1:30000/";
+    extraConfig = ''
+      RewriteEngine On
+
+      RewriteCond %{HTTP:Upgrade} =websocket [NC]
+      RewriteRule /(.*)           ws://127.0.0.1:30000/$1 [P,L]
+      RewriteCond %{HTTP:Upgrade} !=websocket [NC]
+      RewriteRule /(.*)           http://127.0.0.1:30000/$1 [P,L]
+
+      ProxyPass / http://127.0.0.1:30000/
+      ProxyPassReverse / http://127.0.0.1:30000/
+    '';
   };
 }
