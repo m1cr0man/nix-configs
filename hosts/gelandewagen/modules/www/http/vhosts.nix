c3e038b35f5f4ab5447c511a9984367fb60a56b1
Lucas Savva <lucas@m1cr0man.com>
2022-01-09 02:17:55 +0000

    Set up another cert for conor foundry

--- hosts/gelandewagen/modules/www/http/vhosts.nix
+++ hosts/gelandewagen/modules/www/http/vhosts.nix
@@ -10,9 +10,15 @@ with lib.m1cr0man;
 
     "breogan.${domain}" = makeVhostProxy { host = "127.0.0.1:1357"; };
 
-    "foundry.conor.${domain}" = makeVhostProxy { host = "127.0.0.1:30000"; };
-
     "eggnor.m1cr0man.com" = makeVhostProxy { host = "127.0.0.1:5120"; };
 
+    # Subdomain can't use wildcard certs
+    "foundry.conor.${domain}" = (makeVhostProxy { host = "127.0.0.1:30000"; }) // {
+      useACMEHost = null;
+      enableACME = true;
+    };
+
   };
+
+  security.acme.certs."foundry.conor.${domain}".reloadServices = [ "httpd.service" ];
 }
