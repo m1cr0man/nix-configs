c7005f1e878226d4a098b680be591c268de70a5d
Lucas Savva <lucas@m1cr0man.com>
2022-06-06 21:56:04 +0100

    unimog: Add gnupg to lucas env

--- hosts/unimog/modules/users.nix
+++ hosts/unimog/modules/users.nix
@@ -11,7 +11,10 @@ in
 
   users.users = with lib.m1cr0man; lib.mkMerge [
     (makeNormalUser "lucas" {
-      extraArgs.extraGroups = [ "wheel" ];
+      extraArgs = {
+        extraGroups = [ "wheel" ];
+        packages = [ pkgs.gnupg ];
+      };
       keys = rootKeys ++ [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIq17Vz/gxVGDifkRFO6W5DJvJ5JnZ+DBq85W3UtRv82 lucas@ip-svr"
       ];
