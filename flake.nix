94faf094f03f74a25bbdb9d04ee82cd599ff5602
Lucas Savva <lucas@m1cr0man.com>
2022-06-13 00:35:03 +0100

    Update nixpkgs to rebase of nixos-containers poc

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "M1cr0man Nix Configurations";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "path:/home/lucas/ssd/nixpkgs";
 
     deploy-rs.url = github:serokell/deploy-rs;
     deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
