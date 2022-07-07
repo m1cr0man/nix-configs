9c041500869c7b7f08628eb06a249fe4924042b6
Lucas Savva <lucas@m1cr0man.com>
2022-07-07 20:00:21 +0100

    Reinvent the wheel on nixosSystem

--- lib/output.nix
+++ lib/output.nix
@@ -26,16 +26,20 @@ rec {
     config = { allowUnfree = true; };
   };
 
-  # Found in oxalica's config
-  # Ref: https://github.com/dramforever/config/blob/63be844019b7ca675ea587da3b3ff0248158d9fc/flake.nix#L24-L28
-  systemLabelModule = { lib, ... }: {
-    system.configurationRevision = self.rev or null;
-    # system.nixos.revision = self.rev or null;
-    system.nixos.label =
-      if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
-      then "${lib.substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
-      else lib.warn "Repo is dirty, revision will not be available in system label" "dirty";
-  };
+  systemLabelModule = { lib, ... }:
+    let
+      shortRev = self.shortRev or "dirty";
+      lastMod = lib.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101");
+      label = if self ? shortRev then "${lastMod}.${self.shortRev}" else "dirty";
+    in
+    {
+      # Found ref in oxalica's config + adaptation from nixpkgs.lib.nixosSystem
+      # Ref: https://github.com/dramforever/config/blob/63be844019b7ca675ea587da3b3ff0248158d9fc/flake.nix#L24-L28
+      system.configurationRevision = self.rev or null;
+      system.nixos.revision = self.rev or null;
+      system.nixos.label = label;
+      system.nixos.versionSuffix = ".${label}";
+    };
 
   nixOptionsModule = { pkgs, config, ... }: {
     # Enable flakes globally.
@@ -88,23 +92,23 @@ rec {
     networking.domain = domain;
   };
 
-  # Builds a system configuration entry for nixosConfigurations.
-  # nixpkgs.lib.nixosSystem is defined in nixpkg's flake.nix
-  mkConfiguration =
-    { name, modules ? [ ] }: nixpkgs.lib.nixosSystem {
+  # My own version of nixpkgs.lib.nixosSystem as the latter evaluates
+  # its whole own version of nixpkgs.lib.
+  nixosSystem = modules: import "${nixpkgs.outPath}/nixos/lib/eval-config.nix"
+    {
       # These settings are required to configure the nixpkgs used for module building.
       # Without inheriting pkgs here, the defaultPkgs would be used which wouldn't include
       # our overlays.
       # See https://github.com/NixOS/nixpkgs/blob/8377a7bca967dba811899164d4218d1d4a24b483/nixos/lib/eval-config.nix#L43
       # and https://github.com/NixOS/nixpkgs/blob/0699530f08290f34c532beedd66046825d9756fa/nixos/modules/misc/nixpkgs.nix#L58
-      inherit system pkgs;
+      inherit system pkgs modules;
       inherit (pkgs) lib;
+    };
 
-      # args is technically deprecated/internal.
-      # you shouldn't use them here (although you could).
-      # instead, create a module and set _module.args = { ... }
-
-      modules = modules ++ [
+  # Builds a system configuration entry for nixosConfigurations.
+  mkConfiguration =
+    { name, modules ? [ ] }: nixosSystem (
+      modules ++ [
         systemLabelModule
         (baseModule name)
         nixOptionsModule
@@ -114,25 +118,23 @@ rec {
         "global-options.nix"
         "secrets"
         "sysconfig"
-      ]);
-    };
+      ])
+    );
 
+  # Builds a container configuration entry for nixosContainers
   mkContainer =
     { name, modules ? [ ] }:
     let
-      container = nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        inherit (pkgs) lib;
-
-        modules = modules ++ [
+      container = nixosSystem (
+        modules ++ [
           (baseModule name)
           "${configPath}/containers/${name}/configuration.nix"
         ] ++ (pkgs.lib.m1cr0man.module.addModules myModulesPath [
           "containers"
           "secrets"
           "sysconfig/users-groups.nix"
-        ]);
-      };
+        ])
+      );
     in
     pkgs.buildEnv {
       inherit name;
