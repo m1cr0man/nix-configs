beb485253285cd00a1418815a05616519318a6ef
Lucas Savva <lucas@m1cr0man.com>
2022-07-09 02:10:24 +0100

    plex: Change data path

--- modules/www/plex.nix
+++ modules/www/plex.nix
@@ -2,13 +2,11 @@
   services.plex = {
     enable = true;
     openFirewall = false;
-    dataDir = "/var/lib/www/plex";
   };
 
   # Only allow important ports
   networking.firewall.allowedTCPPorts = [ 32400 ];
 
-  systemd.services.plex.requires = [ "var-lib-plex.mount" ];
   systemd.services.plex.after = [ "var-lib-plex.mount" "acme-m1cr0man.com.service" ];
 
   systemd.services.plex.serviceConfig.UMask = "0077";
