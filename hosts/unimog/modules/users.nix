7824929853be89095210c7b30f3931f4bd44dc0c
Lucas Savva <lucas@m1cr0man.com>
2023-01-17 22:06:21 +0000

    unimog: Add portfwd-guest

--- hosts/unimog/modules/users.nix
+++ hosts/unimog/modules/users.nix
@@ -13,6 +13,7 @@ in
   };
 
   sops.secrets.zeus_password.neededForUsers = true;
+  sops.secrets.portfwd_guest_password.neededForUsers = true;
 
   users.users = with lib.m1cr0man; lib.mkMerge [
     (makeNormalUser "anders" {
@@ -48,5 +49,13 @@ in
       };
     })
 
+    (makeNormalUser "portfwd-guest" {
+      home = "/var/empty";
+      group = "nogroup";
+      keys = rootKeys ++ [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtXDL7LWBiySe4YZmosFxqzjxjcROtmse22+HFShD4L7bjpqWDkIy7ynTAn/EzizVAT2UFs2z2QObJBsaxObPMdYLpAnVW2sLKh40AhsveYlxiXhVbpfMqIZ6lqtUOMqSN3ql7eUwqWMnWtBz4yl5XwLIoNmnT20XDjNJzoGk+VOTNedldDZEM1oHOw+owtAr1k2sBu2dStXbiUgIjAyDOszNp5z1dyV8Zu/bEmFj3+Uw/JID+IneZCtk/HKrPldwv+tAbSnL2+LTmQhcdfk3GZGRh/EcAyHB+PkswIoxP7p7XoQLt10fdYYpzPur4Mo45gH/RE9ybhpxfasAj7411w== git@ip"
+      ];
+      extraArgs.passwordFile = config.sops.secrets.portfwd_guest_password.path;
+    })
   ];
 }
