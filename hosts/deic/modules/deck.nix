7801cceb10c128514761af0756ddd34ba4cd5c18
Lucas Savva <lucas@m1cr0man.com>
2023-05-18 23:02:43 +0100

    hosts/deic: Update flake lock + config for 6.1.2 kernel

--- hosts/deic/modules/deck.nix
+++ hosts/deic/modules/deck.nix
@@ -2,29 +2,16 @@
 {
   jovian = {
     devices.steamdeck.enable = true;
-    devices.steamdeck.enableSoundSupport = true;
-    devices.steamdeck.enableMesaPatches = false;
     steam.enable = true;
   };
 
+  # Use pipewire + wireplumber for all audio
+  hardware.pulseaudio.enable = false;
+  services.pipewire.wireplumber.enable = true;
+
   services.gnome.gnome-remote-desktop.enable = false;
 
-  # Audio stuff
-  hardware.pulseaudio.enable = false;
-  services.pipewire.enable = true;
-  services.pipewire.wireplumber.enable = true;
   environment.systemPackages = [
-    (pkgs.alsa-ucm-conf.overrideAttrs (prev: {
-      # This dumb import breaks all the audio on the Deck itself.
-      # Best sources as to why I could find:
-      # https://github.com/alsa-project/alsa-ucm-conf/issues/104
-      # see comments: https://github.com/alsa-project/alsa-ucm-conf/commit/1e6297b650114cb2e043be4c677118f971e31eb7
-      postInstall = ''
-        ${pkgs.gnused}/bin/sed -i /Include.libgen.File/d $out/share/alsa/ucm2/ucm.conf
-      '';
-      meta.priority = -10;
-    }))
-
     pkgs.gnome.gnome-tweaks
     # Tray icons
     pkgs.gnomeExtensions.appindicator
@@ -60,7 +47,6 @@
     evince # document viewer
     gnome-software
     gnome-characters
-    totem # video player
     tali # poker game
     iagno # go game
     hitori # sudoku game
@@ -68,13 +54,8 @@
   ]);
 
   environment.defaultPackages = with pkgs; [
-    glxinfo (steam.override { extraArgs = "-steamdeck"; })
+    glxinfo
+    (steam.override { extraArgs = "-steamdeck"; })
     steamdeck-firmware
   ];
-
-  # Updated mesa
-  hardware.opengl = {
-    package = pkgs.mesa_23.drivers;
-    package32 = pkgs.pkgsi686Linux.mesa_23.drivers;
-  };
 }
