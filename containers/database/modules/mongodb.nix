9c830a941b06c2ac7fabcfa17d117a2fcfdba397
Lucas Savva <lucas@m1cr0man.com>
2024-02-11 00:19:55 +0000

    databases/mongodb: Add comments

--- containers/database/modules/mongodb.nix
+++ containers/database/modules/mongodb.nix
@@ -10,7 +10,9 @@ in {
   environment.systemPackages = [ pkgs.mongodb-tools ];
 
   systemd.services.mongodb = {
+    # Required so that the service shuts down when no connections remain
     bindsTo = [ "mongodb-proxy.service" ];
+    # Required so that the service does not auto-start on boot
     wantedBy = lib.mkForce [];
     serviceConfig = {
       Group = "mongodb";
@@ -22,8 +24,11 @@ in {
     description = "Connects clients to mongodb via systemd sockets";
 
     # Derived from https://www.man7.org/linux/man-pages/man8/systemd-socket-proxyd.8.html#EXAMPLES
+    # bindsTo used so that if the socket unit is stopped, so is this.
     bindsTo = [ "mongodb-proxy.socket" ];
+    # Required to start mongodb. Can't use bindsTo because we used it in mongodb (circular ref).
     requires = [ "mongodb.service" ];
+    # Both the socket and the service need to be online before the proxy begins
     after = [ "mongodb-proxy.socket" "mongodb.service" ];
 
     unitConfig.JoinsNamespaceOf = "mongodb.service";
@@ -43,6 +48,7 @@ in {
     listenStreams = [ "/var/lib/sockets/mongodb.sock" ];
 
     socketConfig = {
+      # One proxy service per listener connection
       Accept = false;
       SocketUser = "mongodb";
       SocketGroup = "sockets";
