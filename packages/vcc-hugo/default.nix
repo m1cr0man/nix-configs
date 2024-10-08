306df692184b909fd4e3c0ac795ed79cc9d886e6
Lucas Savva <lucas@m1cr0man.com>
2024-10-08 22:13:07 +0100

    packages/vcc-hugo: Fix compilation

--- packages/vcc-hugo/default.nix
+++ packages/vcc-hugo/default.nix
@@ -1,13 +1,13 @@
 { stdenvNoCC, hugo, git, openssh }:
 stdenvNoCC.mkDerivation {
   name = "vcc-hugo";
-  version = "1.1.0";
+  version = "1.1.1";
 
   src = builtins.fetchGit {
     url = "george@phoenix.vccomputers.ie:/home/george/vcc-hugo.git";
     name = "vcc-hugo";
     submodules = true;
-    rev = "2dbf1326da81d4e96a33fd8408ab5c88e28cdbc7";
+    rev = "9f77be2e66f641d904f115c98c176d98bd1d387e";
   };
 
   buildInputs = [ hugo git openssh ];
