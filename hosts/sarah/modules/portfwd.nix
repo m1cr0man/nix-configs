9417cc9808f8739e9e4a7091de6c0ced08ea6aab
Lucas Savva <lucas@m1cr0man.com>
2025-02-03 16:53:24 +0000

    hosts/sarah: Port forward over Tailscale

--- hosts/sarah/modules/portfwd.nix
+++ hosts/sarah/modules/portfwd.nix
@@ -5,7 +5,7 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" "tailscale.service" ];
     serviceConfig = {
-      ExecStart = "${pkgs.socat}/bin/socat TCP-LISTEN:25565,fork,reuseaddr TCP:phoenix.george:25565";
+      ExecStart = "${pkgs.socat}/bin/socat TCP-LISTEN:25565,fork,reuseaddr TCP:phoenix.george.ts.m1cr0man.com:25565";
       Restart = "always";
       RestartSec = 10;
     };
